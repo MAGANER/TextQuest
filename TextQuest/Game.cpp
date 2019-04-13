0fbbcece62296e839be2a8dcb94a2f53a95536a3
MAGANER <maganer.krik@mail.ru>
2019-04-13 14:46:41 +0300

    added method that draws main menu

--- TextQuest/Game.cpp
+++ TextQuest/Game.cpp
@@ -6,11 +6,60 @@ void Game::run()
 	{
 		load_scenes();
 	}
+	
+	switch (current_state)
+	{
+	case GameState::game:
+		run_game();
+		break;
+	case GameState::main_menu:
+		run_start();
+		break;
+	case GameState::death:
+		run_death();
+		break;
+	}
 
-	auto scene = scenes.begin();
     _getch();
 }
+void Game::run_game()
+{
 
+}
+void Game::run_death()
+{
+
+}
+void Game::run_start()
+{
+	system("cls");
+	draw_start();
+}
+   
+void Game::draw_start()
+{
+	cout <<"    " << "######### ######### #         #  ###########" << endl;
+	cout << "   " << "    #     #          ##     ##        #     " << endl;
+	cout << "   " << "    #     #            #   #          #     " << endl;
+	cout << "   " << "    #     #########     # #           #     " << endl;
+	cout << "   " << "    #     #              #            #     " << endl;
+	cout << "   " << "    #     #           #    #          #     " << endl;
+	cout << "   " << "    #     #          #      #         #     " << endl;
+	cout << "   " << "    #     ######### #        #        #     " << endl;
+	cout << endl;
+	cout << "#########  #       #    #########  ###########   ##########" << endl;
+	cout << "#       #  #       #    #          #                  #    " << endl;
+	cout << "#       #  #       #    #          #                  #    " << endl;
+	cout << "#       #  #       #    #########  ###########        #    " << endl;
+	cout << "#       #  #       #    #                    #        #    " << endl;
+	cout << "#       #  #       #    #                    #        #    " << endl;
+	cout << "#########  #########    #########  ###########        #    " << endl;
+	cout << "       ####                                                " << endl;
+	cout << endl << endl;
+	cout << "               type   START to start:                      " << endl;
+
+
+}
 void Game::load_scenes()
 {
 	int max_scene_number = get_max_scene_number();
@@ -42,6 +91,7 @@ int Game::get_max_scene_number()
 Game::Game()
 {
 	scenes_are_loaded = false;
+	current_state = GameState::main_menu;
 }
 
 
--- TextQuest/Game.h
+++ TextQuest/Game.h
@@ -7,15 +7,26 @@
 #pragma once
 #include"Scene.h"
 #include<conio.h>
+
+enum GameState
+{
+	game = 1,
+	death,
+	main_menu
+};
+
 class Game
 {
 private:
 	bool scenes_are_loaded;
+	int current_state;
 
 	vector<Scene*> scenes;
 private:
 	void run_game();
 	void run_death();
+
+	void draw_start();
 	void run_start();
 
 	void load_scenes();
